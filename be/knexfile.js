a9dd91b78c51f90b24f0ba886680abc585b8aeee
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-26 19:57:10 -0300

    Setup knexfile.js

---
+++ be/knexfile.js
@@ -0,0 +1,37 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'base',
+      user:     'user',
+      password: 'pass'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'base',
+      user:     'user',
+      password: 'pass'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
