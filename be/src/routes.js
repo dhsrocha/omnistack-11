54869a87111d7bf4a90ec8531f658911e1d37017
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-26 21:33:06 -0300

    Created creation mapping for entity ngo.
    * Also created module for connecting against database.

--- be/src/routes.js
+++ be/src/routes.js
@@ -1,10 +1,28 @@
 "use strict";
 const express = require("express");
+const crypto = require("crypto");
+
+const connection = require("./database/connection");
 
 const router = express.Router();
 
-router.get("/users", (_, res) => {
-  //
+router.post("/ngo", async (req, res) => {
+  const data = req.body;
+  console.info({ "ngo creating request: ": data });
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ngo").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+   res.json({ id });
 });
 
 module.exports = router;
