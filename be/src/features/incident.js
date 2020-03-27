8573a1512c30fea0cf4f7e3b0a80bb4aa81d85ef
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-26 22:19:44 -0300

    Incident feature.

---
+++ be/src/features/incident.js
@@ -0,0 +1,58 @@
+"use strict";
+const connection = require("../database/connection");
+
+module.exports = {
+  async index(_, res) {
+    const data = await connection("incident").select("*");
+    console.info({ "Received retrieving request": data });
+    res.json({ entities: data || {} });
+  },
+  async create(req, res) {
+    const data = req.body;
+    console.info({ "incident creating request: ": data });
+    const { title, description, value } = req.body;
+
+    const ngo_id = auth(req, res);
+
+    const [id] = await connection("incident")
+      .insert({
+        title,
+        description,
+        value,
+        ngo_id
+      })
+      .returning("id");
+
+    return res.json({ id });
+  },
+  async delete(req, res) {
+    const { id } = req.params;
+    console.info({ "Received deleting request for id: ": id });
+
+    const ngo_id = auth(req, res);
+
+    const incident = await connection("incident")
+      .where("id", id)
+      .select("ngo_id")
+      .first();
+
+    if (incident.ngo_id !== ngo_id)
+      res.status(401).json({ error: "Operation not permitted." });
+
+    await connection("incident")
+      .where("id", id)
+      .delete();
+
+    return res.status(204).send();
+  }
+};
+
+function auth(req, res) {
+  const ngo_id = req.headers.authorization;
+  if (!ngo_id)
+    res
+      .status(401)
+      .json({ message: "Absent authorization header." })
+      .send();
+  return ngo_id;
+}
--- be/src/routes.js
+++ be/src/routes.js
@@ -1,10 +1,15 @@
 "use strict";
 const express = require("express");
-const ngo = require("./features/ngo")
+const ngo = require("./features/ngo");
+const incident = require("./features/incident");
 
 const router = express.Router();
 
 router.get("/ngo", ngo.index);
 router.post("/ngo", ngo.create);
+
+router.get("/incident", incident.index);
+router.post("/incident", incident.create);
+router.delete("/incident/:id", incident.delete);
 
 module.exports = router;
