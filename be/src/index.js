7d610c7e9138e96c7de74dffea0973cd0dd54e93
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-27 00:21:39 -0300

    Added CORS dependency package.
    * Also included as express' middleware.

--- be/src/index.js
+++ be/src/index.js
@@ -1,9 +1,13 @@
 "use strict";
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express(routes);
-app.use(express.json())
+app.use(express.json());
+app.use(cors({
+  // https:some-site-to-deploy.com.br # in case of production environments.
+}));
 
 app.use(routes);
 app.listen(3333);
