9b075f28a88e4283f42f8cca9df291e22e35114f
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-27 14:46:14 -0300

    Incremented counter increment/decrement.
    Although intentionally it doesn't work in React rendering context.

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,7 +2,27 @@ import React from "react";
 import Header from "./Header";
 
 function App() {
-  return <Header>Be the Hero.</Header>;
+  let counter = 0;
+  const increment = () => {
+    counter++;
+    console.info({ counter: counter });
+    return counter;
+  };
+  const decrement = () => {
+    counter--;
+    console.info({ counter: counter });
+    return counter;
+  };
+  return (
+    <div>
+      <Header>Be the Hero.</Header>
+      <span>
+        <h2>Counter: {counter}</h2>
+      </span>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
 }
 
 export default App;
