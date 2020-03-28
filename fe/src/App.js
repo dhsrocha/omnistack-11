65d13be9ada10a2071b78669d0a39637a689c282
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-28 19:54:57 -0300

    Added Logon page component.
    * Replaced Header.js in client component with this;
    * Added corresponding CSS script;

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,23 +1,12 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React from "react";
+
+import Logon from "./pages/Logon";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const increment = () => {
-    setCounter(counter + 1 );
-  };
-  const decrement = () => {
-    setCounter(counter - 1 );
-  };
   return (
-    <div>
-      <Header>Be the Hero.</Header>
-      <span>
-        <h2>Counter: {counter}</h2>
-      </span>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-    </div>
+    <>
+      <Logon />
+    </>
   );
 }
 
