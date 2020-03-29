a269590a0e0fb6523c005f6e4d7272dc9c6ffc1d
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-29 16:20:33 -0300

    Added Routes component into mobile module.
    * Applied imported pages folder's components as pages.
    * Included into App.js as client code

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,5 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
+import Detail from "./pages/Detail";
+import Incident from "./pages/Incident";
+
+const AppStack = createStackNavigator();
 
 export default function Routes() {
-
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Incident" component={Incident} />
+        <AppStack.Screen name="Detail" component={Detail} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
 }
