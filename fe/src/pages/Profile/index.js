d7fbfee5367fc06483f88285fafbb56cf966cb07
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-29 01:06:36 -0300

    Profile component's list items.

--- fe/src/pages/Profile/index.js
+++ fe/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FiPower } from "react-icons/fi";
+import { FiPower, FiTrash2 } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import "./styles.css";
@@ -18,6 +18,58 @@ export default function Profile() {
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
+
+      <h1>Casos cadastrados</h1>
+
+      <ul>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+          <button type="button">
+            <FiTrash2 size={20} color="a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+          <button type="button">
+            <FiTrash2 size={20} color="a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+          <button type="button">
+            <FiTrash2 size={20} color="a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+          <button type="button">
+            <FiTrash2 size={20} color="a8a8b3" />
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
