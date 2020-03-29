9c68d7b8a3f90025c522f742f6b15c2ebd9bfbbd
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-28 23:15:25 -0300

    Expanded Register component.

--- fe/src/pages/Register/index.js
+++ fe/src/pages/Register/index.js
@@ -1,5 +1,39 @@
 import React from "react";
+import { FiArrowLeft } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import "./styles.css";
 
 export default function Register() {
-  return <h1>Register</h1>
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logo} alt="'Be the Hero" />
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude as pessoas a
+            encontrarem os casos da sua ONG.
+          </p>
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Nome da ONG" />
+          <input type="email" placeholder="Email" />
+          <input placeholder="Whatsapp" />
+          <div className="input-group">
+            <input placeholder="Cidade" />
+            <input placeholder="UF" style={{ width: 80 }} />
+          </div>
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
