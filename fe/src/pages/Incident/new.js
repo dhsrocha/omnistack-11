e0638218039d76b356a6e906257fef3ca487d221
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-29 01:42:04 -0300

    Expanded 'NewIncident' component.
    * Copied CSS rules from Register component's rules; and
    * Added text area's  CSS rules to the global scope.

--- fe/src/pages/Incident/new.js
+++ fe/src/pages/Incident/new.js
@@ -1,7 +1,34 @@
 import React from "react";
-
+import { FiArrowLeft } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
 import "./styles.css";
 
 export default function NewIncident() {
-  return <h1>New Incident</h1>;
+  return (
+    <div className="incident-container">
+      <div className="content">
+        <section>
+          <img src={logo} alt="'Be the Hero" />
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhadamente para que seu heroi possa resolvê-lo.
+          </p>
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar à página inicial
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Título do caso" />
+          <textarea placeholder="Descrição do caso" />
+          <input placeholder="Valor em reais" />
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
