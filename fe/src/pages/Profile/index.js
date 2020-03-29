4e93b9feed9fc0cb6ce4048f1fcaf596336b6e67
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-29 00:43:58 -0300

    Header section of Profile component.

--- fe/src/pages/Profile/index.js
+++ fe/src/pages/Profile/index.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import "./styles.css";
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  return (
+    <div className="profile-container">
+      <header>
+        <img src={logo} alt="'Be the Hero" />
+        <span>Bem vindo, APAD</span>
+
+        <Link className="button" to="/incident/new">
+          Cadastrar novo caso
+        </Link>
+      </header>
+    </div>
+  );
 }
