e224e98b6016fd4b2027b53858afd94843c6fb08
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-29 17:31:37 -0300

    Added Header section of mobile module's Incident component.
    * Also included styles for this part.

--- mobile/src/pages/Incident/index.js
+++ mobile/src/pages/Incident/index.js
@@ -1,7 +1,23 @@
 import React from "react";
-import { View } from "react-native";
-// import styles from "./styles";
+import { View, Image, Text } from "react-native";
+import styles from "./styles";
+import logo from "../../assets/logo.png";
 
 export default function Incident() {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logo} />
+        <Text style={styles.container}>
+          Total de <Text styles={styles.headerTextBold}>0 casos</Text>.
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem vindo!</Text>
+      <Text style={styles.description}>
+        Escolha um dos casos abaixo e salve o dia.
+      </Text>
+
+    </View>
+  );
 }
