834348ed8492ba6b95329cc9c7612de1753f8217
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-29 03:17:56 -0300

    Added submission logic to Logon component.

--- fe/src/pages/Logon/index.js
+++ fe/src/pages/Logon/index.js
@@ -1,20 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import { FiLogIn } from "react-icons/fi";
-
+import { Link, useHistory } from "react-router-dom";
+import heroes from "../../assets/heroes.png";
+import logo from "../../assets/logo.svg";
+import api from "../../services/api";
 import "./styles.css";
 
-import logo from "../../assets/logo.svg";
-import heroes from "../../assets/heroes.png";
-
 export default function Logon() {
+  // Hooks
+  const history = useHistory();
+
+  const [id, setId] = useState("");
+
+  // Handlers
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.info({ "Handling login submission:": id });
+    api
+      .post("session", { id })
+      .then(r => {
+        localStorage.setItem("ngo-id", id);
+        localStorage.setItem("ngo-name", r.data.name);
+        history.push("/profile");
+      })
+      .catch(err => alert("Erro no login", err));
+  };
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logo} alt="'Be the Hero" />
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Faça seu login</h1>
-          <input placeholder="Sua identificação." />
+          <input
+            placeholder="Sua identificação."
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">
             Entrar
           </button>
