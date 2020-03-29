1e53f33727f4cb510bc665f2608d079d02281d48
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-03-29 11:51:42 -0300

    Added retrieving logic to Profile component.
    * Used 'useEffect' hook to handle retrieving request;
    * Replaced hardcoded entries by the request data ones; and
      * Also applied formatting to "value"'s field.

--- fe/src/pages/Profile/index.js
+++ fe/src/pages/Profile/index.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FiPower, FiTrash2 } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
+import api from "../../services/api";
 import "./styles.css";
 
 export default function Profile() {
+  // Local storage
+  const id = localStorage.getItem("ngo-id");
+  const name = localStorage.getItem("ngo-name");
+
+  // Hooks
+  const [incidents, setIncidents] = useState([]);
+
+  // * Triggers the first parameter's callback when the second one's state changes.
+  useEffect(() => {
+    api
+      .get("profile", { headers: { Authorization: id } })
+      .then(r => setIncidents(r.data.entities))
+      .catch(err => console.error("Erro ao obter incidentes", err));
+  }, [id, name]);
+
   return (
     <div className="profile-container">
       <header>
         <img src={logo} alt="'Be the Hero" />
-        <span>Bem vindo, APAD</span>
+        <span>Bem vindo, {name}</span>
 
         <Link className="button" to="/incident/new">
           Cadastrar novo caso
@@ -22,53 +38,24 @@ export default function Profile() {
       <h1>Casos cadastrados</h1>
 
       <ul>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-          <button type="button">
-            <FiTrash2 size={20} color="a8a8b3" />
-          </button>
-        </li>
-
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-          <button type="button">
-            <FiTrash2 size={20} color="a8a8b3" />
-          </button>
-        </li>
-
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-          <button type="button">
-            <FiTrash2 size={20} color="a8a8b3" />
-          </button>
-        </li>
-
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-          <button type="button">
-            <FiTrash2 size={20} color="a8a8b3" />
-          </button>
-        </li>
+        {incidents.map(i => (
+          <li key={i.id}>
+            <strong>CASO:</strong>
+            <p>{i.title}</p>
+            <strong>DESCRIÇÃO:</strong>
+            <p>{i.description}</p>
+            <strong>VALOR:</strong>
+            <p>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+              }).format(i.value)}
+            </p>
+            <button type="button">
+              <FiTrash2 size={20} color="a8a8b3" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
